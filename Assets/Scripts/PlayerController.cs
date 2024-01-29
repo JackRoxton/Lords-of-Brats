e56d19184919714fcaf794ab00732ba14cc57b53
QuentinLamy <quentin.lamy16@gmail.com>
2024-01-29 19:29:54 +0100

    pickup with e, throw

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     Vector3 Movement;
-    float speed = 0.5f;
-    float throwStrength = 0.5f;
+    float speed = 10f;
+    float throwStrength = 10f;
+    int hp = 3;
     Vector2 MousePos;
 
     GameObject SavedWeapon;
 
     public GameObject Arm;
+    public GameObject PickupCollision;
+    bool pickingUp;
 
     void Start()
     {
@@ -32,6 +37,17 @@ public class PlayerController : MonoBehaviour
             Throw();
             Debug.Log("Throw");
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(Pickup());
+        }
+
+        if (SavedWeapon != null)
+        {
+            SavedWeapon.transform.position = Arm.transform.position;
+        }
+
     }
 
     private void FixedUpdate()
@@ -40,31 +56,48 @@ public class PlayerController : MonoBehaviour
         Movement.y = Input.GetAxis("Vertical");
 
         Movement *= 0.9f;
-        this.transform.position += Movement * speed;
+        this.transform.position += Movement * speed * Time.deltaTime;
 
         MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Debug.Log(MousePos);
     }
 
+    public void GetHit()
+    {
+        hp -= 1;
+        if(hp <= 0)
+        {
+            Debug.Log("ono am ded");
+        }
+    }
+
     void Attack()
     {
-        //
+        Arm.GetComponent<Animator>().Play("WeaponAttack");
     }
 
     void Throw()
     {
         if (SavedWeapon == null) return;
-        GameObject go;
-        go = Instantiate(SavedWeapon, Arm.transform.position, Quaternion.identity);
-        go.GetComponent<Rigidbody2D>().velocity = new Vector2(MousePos.x - this.transform.position.x,MousePos.y - this.transform.position.y);
+        SavedWeapon.GetComponent<Rigidbody2D>().velocity = new Vector2(MousePos.x - this.transform.position.x,MousePos.y - this.transform.position.y).normalized * throwStrength;
         SavedWeapon = null;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    IEnumerator Pickup()
     {
-        if(collision.gameObject.tag == "Pickable")
+        PickupCollision.SetActive(true);
+        pickingUp = true;
+        yield return new WaitForSeconds(1);
+        PickupCollision.SetActive(false);
+        pickingUp = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision == null) return;
+
+        if (collision.gameObject.tag == "Pickable" && pickingUp)
         {
-            Debug.Log(collision.gameObject.name);
             SavedWeapon = collision.gameObject;
             SavedWeapon.tag = "Weapon";
         }
